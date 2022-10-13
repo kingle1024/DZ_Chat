f1bcf2c1c5d3a22e22b8e4dbfe63317eb48f9e87
최수진 <tnwlssl0729@naver.com>
2022-10-13 21:17:06 +0900

    sj jh view

--- DZ_Chat/src/core/client/View.java
+++ DZ_Chat/src/core/client/View.java
@@ -1,6 +1,6 @@
 package core.client;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class View {
 	String str;
@@ -14,7 +14,25 @@ public class View {
 	public void printMenu() {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 1; i <= menu.size(); i++) {
-			
+			sb.append("[")
+			.append(i)
+			.append("]")
+			.append(str);
+		}
+		System.out.println(sb.toString());
+	}
+	public static void main(String[]args) {
+		View main = new View("main");
+		main.menu.add(new View("login"));
+		main.menu.add(new View("register"));
+		main.menu.add(new View("findpw"));
+		
+		View ptr = main;
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			ptr.printMenu();
+			int num = scanner.nextInt();
+			ptr = ptr.menu.get(num-1);
 		}
 	}
 }
