c48fff63d3db89327b0400a5069ddbf81e0c4cc3
최수진 <tnwlssl0729@naver.com>
2022-10-14 17:34:34 +0900

    member file연동

--- DZ_Chat/src/member/MemberDelete.java
+++ DZ_Chat/src/member/MemberDelete.java
@@ -1,10 +1,15 @@
 package member;
 
+import java.io.FileOutputStream;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import message.ftp.FileCommon;
 
 public class MemberDelete {
 
 	MemberDao dao;
+	FileCommon fileCommon;
 
 	public void delete(Scanner scanner) {
 		try {
@@ -13,6 +18,7 @@ public class MemberDelete {
 			int check;
 
 			dao = new MemberDao();
+			fileCommon = new FileCommon();
 
 			System.out.println("\n2. 회원 탈퇴");
 			System.out.println("본인 확인");
@@ -28,9 +34,8 @@ public class MemberDelete {
 			check = scanner.nextInt();
 
 			if (1 == check) {
-				if (dao.deleteMember(uid))
-					dao.fileSave();
-					System.out.println("탈퇴되었습니다.");
+				deleteFile(uid);
+				System.out.println("탈퇴되었습니다.");
 			} else if (2 == check) {
 				System.out.println("취소되었습니다.");
 			}
@@ -49,4 +54,19 @@ public class MemberDelete {
 			return true;
 		}
 	}
+
+	private void deleteFile(String uid) throws Exception {
+
+		Map<String, Member> memberMap = dao.deleteMember(uid);
+		
+		//파일 비우기
+		new FileOutputStream("./memberFile.txt").close();
+		
+		//map에 있는 모든 값 파일에 적기
+		for (Entry<String, Member> entry : memberMap.entrySet()) {
+			fileCommon.saveContent("./memberFile.txt", entry.getValue() + "\n", true);
+		}
+
+	}
+
 }
