9eccb7bb39831fe90842cddfdb3a74e4821bf47e
kingle1024 <teran1024@naver.com>
2022-10-18 01:44:33 +0900

    채팅방 생성 로그 추가

--- DZ_Chat/src/core/service/chat/MakeChatRoomService.java
+++ DZ_Chat/src/core/service/chat/MakeChatRoomService.java
@@ -1,22 +1,32 @@
 package core.service.chat;
 
 import java.io.*;
-import java.net.Socket;
-
 import core.server.MainServer;
 import core.service.ObjectStreamService;
+import log.Log;
+import log.NeedLog;
 import message.chat.ChatRoom;
 
-public class MakeChatRoomService extends ObjectStreamService {
+public class MakeChatRoomService extends ObjectStreamService implements NeedLog {
 	public MakeChatRoomService(ObjectInputStream is, ObjectOutputStream os) throws IOException {
 		super(is, os);
 	}
+	private String chatRoomName;
 	@Override
 	public void request() throws IOException {
 		try {
 			String chatRoomName = (String) is.readObject();
+			this.chatRoomName = chatRoomName;
 			MainServer.chatRoomMap.put(chatRoomName, new ChatRoom(chatRoomName));
+
 		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public Log toLog() {
+		String logMessage = "MakeChatRoom : " + this.chatRoomName;
+		return new Log("chatlog.txt", logMessage);
 	}
 }
