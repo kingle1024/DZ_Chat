d44cc72a44643e7d0fce09a208311fb362953d86
kingle1024 <teran1024@naver.com>
2022-10-18 01:46:46 +0900

    - property 파일 적용 - chat.starts 분기 추가(fileSend, fileSave)

--- DZ_Chat/src/message/chat/FileMessage.java
+++ DZ_Chat/src/message/chat/FileMessage.java
@@ -1,6 +1,8 @@
 package message.chat;
 
-//import message.ftp.ClientToServer;
+import message.ftp.ClientToServerThread;
+import message.ftp.FtpClient;
+import property.Property;
 
 import java.io.IOException;
 import java.net.Socket;
@@ -16,7 +18,9 @@ public class FileMessage {
         String chatRoomName = (String) map.get("chatRoomName");
 
         // FTP Client
-        Socket socket = new Socket("localhost", 55_552);
+        Socket socket = new Socket(
+                Property.list().get("IP"),
+                Integer.parseInt(Property.list().get("FTP_PORT")));
         StringBuilder input = new StringBuilder();
         input.append(chat)
                 .append(" ")
@@ -27,15 +31,23 @@ public class FileMessage {
 
         HashMap<String, Object> threadMap = new HashMap<>();
         String[] message = input.toString().split(" ");
-        System.out.println("FileMessage > run");
+//        System.out.println("FileMessage > run");
         String fileName = message[1];
 
         threadMap.put("fileName", input.toString());
         threadMap.put("socket", socket);
+        threadMap.put("chatRoomName", chatRoomName);
 
-//        ClientToServer clientToServer =
-//                new ClientToServer(threadGroup, fileName, threadMap);
-//        clientToServer.start();
+        if(chat.startsWith("#fileSend")) {
+            ClientToServerThread clientToServerThread =
+                    new ClientToServerThread(threadGroup, fileName, threadMap);
+            clientToServerThread.start();
+        }else if(chat.startsWith("#fileSave")){
+            threadMap.put("chat", chat);
+            FtpClient ftpClient =
+                    new FtpClient(threadGroup, fileName, threadMap);
+            ftpClient.start();
+        }
 
         // #fileStop aa
         if(chat.startsWith("#fileStop")) {
