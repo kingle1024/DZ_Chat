dc8a0ac711c7db34de19f8ea5660583cb2a63ddf
kingle1024 <teran1024@naver.com>
2022-10-18 01:48:59 +0900

    fileBalance 추가 - 파일명을 리턴해줌 - 폴더를 만들어줌

--- DZ_Chat/src/message/ftp/FileCommon.java
+++ DZ_Chat/src/message/ftp/FileCommon.java
@@ -1,30 +1,63 @@
 package message.ftp;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FileCommon {
 	/**
 	 * 
-	 * @param filePath
-	 * @param message
 	 * @param isAppend true이면 연속해서 파일 작성, false이면 새로 파일 작성
-	 * @return
 	 */
-	public boolean saveContent(String filePath, String message, boolean isAppend) {		
-		try{            
-            FileOutputStream f = new FileOutputStream(filePath, isAppend);              
-            String lineToAppend = message;
-            byte[] byteArr = lineToAppend.getBytes(); //converting string into byte array
-            f.write(byteArr);
-            f.close();
-            
-            //채팅시간, 유저아디@ip, 채팅내용            
-        }
-        catch(Exception e){
-            System.out.println(e);
-            return false;
-        }
+	public boolean saveContent(String filePath, String message, boolean isAppend) {
+		try {
+			FileOutputStream f = new FileOutputStream(filePath, isAppend);
+			byte[] byteArr = message.getBytes(); //converting string into byte array
+			f.write(byteArr);
+			f.close();
+
+			//채팅시간, 유저아디@ip, 채팅내용
+		} catch (IOException e) {
+			System.out.println(e);
+			e.printStackTrace();
+			return false;
+		}
 
 		return true;		
+	}
+	public String fileNameBalance(String path, String fileName) {
+		String[] files;
+		try {
+			files = fileName.split("\\.");
+		}catch(Exception e) {
+			System.out.println(fileName + " : 파일명이 올바르지 않습니다. (Ex test.txt) ");
+			return null;
+		}
+
+		File file = new File(fileName);
+		fileName = file.getName();
+
+		int idx = 1;
+		StringBuilder sbFileName = new StringBuilder(path + fileName);
+
+		file = new File(path);
+		if(!new File(path).exists()) {
+			file.mkdirs();
+		}
+
+		while(true) {
+			file = new File(sbFileName.toString());
+
+			if (file.exists()) {
+				sbFileName = new StringBuilder(
+						path + files[0] + " ("+idx+")." + files[1]);
+				idx++;
+			}else {
+				break;
+			}
+		}
+
+		System.out.println("사용할 파일위치와 파일명 : "+sbFileName.toString());
+		return sbFileName.toString();
 	}
 }
