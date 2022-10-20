887338880485d86de9826413f9974791111cc373
jhkim988 <fccva3910@gmail.com>
2022-10-21 07:28:13 +0900

    chatServiceList 이름 변경 입장 퇴장 메서드 개선

--- DZ_Chat/src/message/chat/ChatRoom.java
+++ DZ_Chat/src/message/chat/ChatRoom.java
@@ -3,32 +3,42 @@ package message.chat;
 import java.io.Serializable;
 import java.util.*;
 
+import core.server.MainServer;
 import core.service.chat.ChatService;
 import member.Member;
 
 public class ChatRoom implements Serializable {
 	private static final long serialVersionUID = 1823559605769244050L;
-	private final String roomName;
-	private final List<ChatService> chatServiceList;
+	private final String chatRoomName;
+	private final List<ChatService> chatServices;
 	
-	public ChatRoom(String roomName) {
-		this.roomName = roomName;
-		this.chatServiceList = new ArrayList<>();
+	public ChatRoom(String chatRoomName) {
+		this.chatRoomName = chatRoomName;
+		this.chatServices = new ArrayList<>();
 	}
 	
 	public void entrance(ChatService chatService) {
-		chatServiceList.add(chatService);
+		chatServices.add(chatService);
+		new SystemMessage(this, chatService.nickname() + "님이 입장하셨습니다. 인원 수: " + size()).push();
 	}
 	
-	public List<ChatService> getChatServiceList() {
-		return chatServiceList;
+	public void exit(ChatService chatService) {
+		chatServices.remove(chatService);
+		new SystemMessage(this, chatService.nickname() + "님이 퇴장하셨습니다. 남은 인원 수: " + size()).push();
+		if (size() == 0) {
+			MainServer.chatRoomMap.remove(chatRoomName);
+		}
+	}
+	
+	public List<ChatService> getChatServices() {
+		return chatServices;
 	}
 	
 	public int size() {
-		return chatServiceList.size();
+		return chatServices.size();
 	}
 	
-	public String getRoomName() {
-		return roomName;
+	public String getChatRoomName() {
+		return chatRoomName;
 	}
 }
