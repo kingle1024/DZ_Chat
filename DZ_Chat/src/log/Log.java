c252f953570e22a7cfa2d5833207defd44c2c16f
jhkim988 <fccva3910@gmail.com>
2022-10-21 11:51:40 +0900

    TimeLog + Comment delete

--- DZ_Chat/src/log/Log.java
+++ DZ_Chat/src/log/Log.java
@@ -1,14 +1,22 @@
 package log;
 
+import java.time.LocalDateTime;
+
 public class Log {
 	private String path;
 	private String log;
+	private LocalDateTime time;
 
 	public Log(String path, String log) {
 		this.path = path;
 		this.log = log;
+		this.time = LocalDateTime.now();
 	}
 
+	public String getTimeLog() {
+		return "[" + time + "]" + log;
+	}
+	
 	public String getPath() {
 		return path;
 	}
--- DZ_Chat/src/log/LogConsumer.java
+++ DZ_Chat/src/log/LogConsumer.java
@@ -11,33 +11,23 @@ public class LogConsumer implements Runnable {
 			while (!Thread.currentThread().isInterrupted()) {
 				appendInfo(logQueue.poll());
 			}
-		} catch (Exception e) {
+		} catch (InterruptedException e) {
 			
 		}
 	}
 	
-	//추후 interface로 뺄 수 있음??(appendinfo interface)
 	public void appendInfo(Log log) {
-		// 파일을 열고
-		// 파일에 내용 추가
-		// 파일 저장
 		System.out.println("Log > appendInfo");
 		try {
 			String filePath = log.getPath();
 			FileOutputStream f = new FileOutputStream(filePath, true);
-//            String lineToAppend = "\r\nThe quick brown fox jumps over the lazy dog";  
-			String lineToAppend = log.getLog();
-			byte[] byteArr = lineToAppend.getBytes(); // converting string into byte array
+			String lineToAppend = log.getTimeLog();
+			byte[] byteArr = lineToAppend.getBytes();
 			f.write(byteArr);
 			f.close();
-
-			// 채팅시간, 유저아디@ip, 채팅내용
-			//
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		// 회원정보, 채팅로그,
-
 	}
 	
 	public void consumeAllLog() {
