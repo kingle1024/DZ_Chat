ce013b2ef151a772ee29078fbd85f045418b91d4
jhkim988 <fccva3910@gmail.com>
2022-11-06 12:17:56 +0900

    UserInfo View 변경

--- DZ_Chat/src/core/client/view/viewimpl/UserInfoView.java
+++ DZ_Chat/src/core/client/view/viewimpl/UserInfoView.java
@@ -7,7 +7,7 @@ import static core.client.Main.*;
 public class UserInfoView extends MenuChooseView {
 
 	public UserInfoView() {
-		super("UserInfo", "회원 비밀번호 수정", "탈퇴");
+		super("UserInfo", "UpdateMember", "DeleteMember");
 	}
 
 	@Override
