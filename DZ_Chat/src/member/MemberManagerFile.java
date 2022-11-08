3ea7e6766837d98a4837031f3b0fe770a7d1d214
jhkim988 <fccva3910@gmail.com>
2022-11-08 13:53:59 +0900

    인터페이스 생성

---
+++ DZ_Chat/src/member/MemberManagerFile.java
@@ -0,0 +1,52 @@
+package member;
+
+public class MemberManagerFile implements MemberManagerInterface {
+
+	public boolean register(Member tmpMember, String pwChk) {
+		if (!tmpMember.validatePw(pwChk)) {
+			System.out.println("회원가입실패:비밀번호불일치");
+			return false;
+		}
+		if (MemberMap.containsKey(tmpMember.getUserId())) {
+			System.out.println("회원가입실패:이미존재하는회원");
+			return false;
+		}
+		MemberMap.put(tmpMember.getUserId(), tmpMember);
+		System.out.println("회원가입성공:" + tmpMember);
+		return true;
+	}
+
+	public Member login(String id, String pw) {
+		if (!MemberMap.containsKey(id)) {
+			return null;
+		}
+		Member member = MemberMap.get(id);
+		if (!member.validatePw(pw))
+			return null;
+		return member;
+	}
+
+	public boolean deleteMember(Member member, String pw) {
+		if (!member.getPassword().equals(pw)) {
+			return false;
+		}
+		MemberMap.deleteMember(member.getUserId());
+		return true;
+	}
+
+	public String findPw(String id) {
+		if (MemberMap.containsKey(id)) {
+			return MemberMap.getpw(id);
+		}
+		return null;
+	}
+
+	public boolean updatePw(Member me, String validatePw, String newPw) {
+		if (!me.validatePw(validatePw)) {
+			return false;
+		}
+		me.setPassword(newPw);
+		MemberMap.put(me.getUserId(), me);
+		return true;
+	}
+}
